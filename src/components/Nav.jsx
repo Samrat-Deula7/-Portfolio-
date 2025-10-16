da382b7e30325317d1ca7e7e4a13992d98b1b9e4
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-16 20:01:25 +0545

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+const Nav=()=> {
+  return (
+    <div className="bg-black w-auto h-10 flex justify-center items-center  ">
+      <ul className="flex gap-3 text-1xl">
+        <li>Home</li>
+        <li>Aboutme</li>
+        <li>Service</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+export default Nav;
