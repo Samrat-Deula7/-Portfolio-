94d9f7901d5c4c1fc8f78e80748154fb3c99726e
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-19 13:18:35 +0545

    Finished working on the loading screen logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Nav from "./components/Nav"
+import { useState } from "react";
+import LoadingScreen from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
-    <Nav/>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded?"opacity-100":"opacity-0"} bg-black text-gray-100`}>
+        <Navbar/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
