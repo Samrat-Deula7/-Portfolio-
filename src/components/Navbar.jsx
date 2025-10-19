682c442a4a4550ad5f92b4f5a4e6d92e6f457971
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-19 20:37:56 +0545

    finished responshive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,56 @@
-const Navbar=()=>{
-return(<>
+import { useEffect } from "react";
 
-</>);
+const Navbar=({menuOpen,setMenuOpen})=>{
+    useEffect(()=>{
+    document.body.style.overflow = menuOpen ? "hidden" : "";
+    },[menuOpen])
+return (
+  <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+    <div className="max-w-5xl mx-auto px-4">
+      <div className="flex justify-between items-center h-16 ">
+        <a href="#home" className="font-mono text-xl font-bold text-white">
+          Samrat <span className="text-blue-500">Dev</span>
+        </a>
+
+        {/* This is the mobile menu */}
+        <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={()=>setMenuOpen((prev)=>!(prev))}>
+          &#9776;
+        </div>
+
+        {/* This is the desktop menu */}
+        {/* In the class hidden sets display to hidden but md:flex sets display to flex from medium size to large */}
+        <div className="hidden md:flex items-center space-x-8">
+          <a
+            href="#home"
+            className="text-gray-300 hover:text-white transition-colors cursor-pointer"
+          >
+            Home
+          </a>
+    
+          <a
+            href="#about"
+            className="text-gray-300 hover:text-white transition-colors cursor-pointer"
+          >
+            About
+          </a>
+       
+          <a
+            href="#projects"
+            className="text-gray-300 hover:text-white transition-colors cursor-pointer"
+          >
+            Projects
+          </a>
+     
+          <a
+            href="#contact"
+            className="text-gray-300 hover:text-white transition-colors cursor-pointer"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
+);
 }
 export default Navbar;
