ae8fac0cdf1e80c2a18f50bd0a12315c47171a43
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-20 22:41:35 +0545

    Fixed the width issue for mobile screen caused by contact section

--- src/components/Sections/Contact.jsx
+++ src/components/Sections/Contact.jsx
@@ -34,10 +34,10 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="min-h-screen flex items-center justify-center py-20"
+      className="min-h-screen flex items-center justify-center py-10"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="px-4 sm:w-[450px] md:w-[800px]">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent text-center">
             Get In Touch
           </h2>
