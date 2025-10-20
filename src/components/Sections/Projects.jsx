b915f92cb19fe203aaa4086cc93c723161ed1604
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-20 11:13:27 +0545

    fimished project section

--- src/components/Sections/Projects.jsx
+++ src/components/Sections/Projects.jsx
@@ -8,7 +8,125 @@ const Projects=()=>{
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent text-center">
             Featured Projects
           </h2>
-          <div></div>
+          <div className="grid  grid-cols-1 md:grid-cols-2 gap-6">
+            {/* Each div is for an project */}
+            <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]  transition-all">
+              <h3 className="text-xl font-bold mb-2">inotebook</h3>
+              <p className="text-gray-400 mb-3">
+                Scalable note storing project with user authentication and user
+                based data retrival
+              </p>
+              <div>
+                {["React", "Node.js", "MongoDB", "Express.js"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)]   transition-all"
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                {/* Need to put link here */}
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project ➜
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]  transition-all">
+              <h3 className="text-xl font-bold mb-2">inotebook</h3>
+              <p className="text-gray-400 mb-3">
+                Scalable note storing project with user authentication and user
+                based data retrival
+              </p>
+              <div>
+                {["React", "Node.js", "MongoDB", "Express.js"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)]   transition-all"
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                {/* Need to put link here */}
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project ➜
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]  transition-all">
+              <h3 className="text-xl font-bold mb-2">inotebook</h3>
+              <p className="text-gray-400 mb-3">
+                Scalable note storing project with user authentication and user
+                based data retrival
+              </p>
+              <div>
+                {["React", "Node.js", "MongoDB", "Express.js"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm  hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)]   transition-all"
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                {/* Need to put link here */}
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project ➜
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]  transition-all">
+              <h3 className="text-xl font-bold mb-2">inotebook</h3>
+              <p className="text-gray-400 mb-3">
+                Scalable note storing project with user authentication and user
+                based data retrival
+              </p>
+              <div>
+                {["React", "Node.js", "MongoDB", "Express.js"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)]  transition-all"
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                {/* Need to put link here */}
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project ➜
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     );
