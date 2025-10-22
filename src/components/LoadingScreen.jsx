9563fbc39d052ef230a946edd70efd79a54d55ef
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-22 12:40:43 +0545

    Improved loading bar

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -24,7 +24,7 @@ const LoadingScreen = ({ onComplete }) => {
       <div className="mb-4  font-mono font-bold text-3xl md:text-5xl lg:text-7xl  bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent ">
         {text} <span className="animate-blink ml-1 text-white">|</span>
       </div>
-      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+      <div className="w-[250px] h-[2px] md:w-[600px] md:h-[4px] bg-gray-800 rounded relative overflow-hidden">
         <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
       </div>
     </div>
