b9f3e62620bd9fbe6e9879e8b7d99d6cf04db8d1
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-22 12:23:24 +0545

    Adding an user Pic

--- src/components/Sections/Home.jsx
+++ src/components/Sections/Home.jsx
@@ -1,4 +1,5 @@
 import RevealOnScroll from "../../components/RevealOnScroll";
+import Pic from "../../assets/pic.png"
 
 const Home = () => {
   return (
@@ -7,8 +8,17 @@ const Home = () => {
       className="min-h-screen flex items-center justify-center relative"
     >
       <RevealOnScroll>
-        <div className="text-center z-10 px-4">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent ">
+        <div className="text-center place-items-center z-10 px-4">
+          <div className="w-43 h-43 lg:w-83 lg:h-83 md:w-53 md:h-53 rounded-full bg-amber-600 mb-20">
+            <div className="w-full h-full flex justify-center items-center ">
+              <img
+                src={Pic}
+                alt="User"
+                className="w-40 h-40 lg:w-80 lg:h-80 md:w-50 md:h-50  bg-cover bg-center rounded-full overflow-clip"
+              />
+            </div>
+          </div>
+          <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent ">
             Hi,This is Home part
           </h1>
           <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
