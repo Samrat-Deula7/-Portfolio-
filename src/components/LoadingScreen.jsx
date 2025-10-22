19cac8144fbe12d543f685b4d5be335aa1997139
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-22 12:36:13 +0545

    Improve loading screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<Hello World/>";
+  const fullText = "Samrat Deula";
 
   useEffect(() => {
     let index = 0;
@@ -21,8 +21,8 @@ const LoadingScreen = ({ onComplete }) => {
 
   return (
     <div className="fixed inset-0 max-w-screen z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-      <div className="mb-4 text-4xl font-mono font-bold">
-        {text} <span className="animate-blink ml-1">|</span>
+      <div className="mb-4  font-mono font-bold text-3xl md:text-5xl lg:text-7xl  bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent ">
+        {text} <span className="animate-blink ml-1 text-white">|</span>
       </div>
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
         <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
