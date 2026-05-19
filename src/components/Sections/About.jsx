e1da030ed59162da91e09ef51c3c478d08c8c059
Samrat-Deula7 <samratdeula461@gmail.com>
2026-05-19 07:37:53 +0545

    Added postgresql to my skills

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -5,6 +5,7 @@ import NodeImg from "../../assets/node.png";
 import ExpressImg from "../../assets/express.png";
 import MongoImg from "../../assets/mongo.png";
 import SQL from "../../assets/SQL_SERVER.png";
+import PostSQL from "../../assets/PostgreSql.png";
 import GitImg from "../../assets/git.png"
 import AwsImg from "../../assets/aws.png"
 import Time from "../../assets/time.gif";
@@ -150,6 +151,7 @@ const About = () => {
                   {scrolled ? "80%" : "0%"}
                 </h4>
               </div>
+
               <div className="relative flex flex-col items-center cursor-pointer gap-y-1 mb-6 w-[140px] h-[200px] justify-center border-white/10 border bg-blue-500/10 px-4 py-6 hover:border-white/50  hover:-translate-y-1 transition-all rounded-2xl ">
                 <div className="h-18">
                   <img
@@ -159,6 +161,27 @@ const About = () => {
                   />
                 </div>
                 <h4 className="text-md ">SQL SERVER</h4>
+                <div className="w-[100px] h-[6px] md:h-[7px] bg-gray-800 rounded  overflow-hidden">
+                  <div
+                    className={`w-0 h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] duration-700  ${
+                      scrolled ? "w-[80%]" : ""
+                    }`}
+                  ></div>
+                </div>
+                <h4 className="duration-700 text-sm">
+                  {scrolled ? "60%" : "0%"}
+                </h4>
+              </div>
+
+              <div className="relative flex flex-col items-center cursor-pointer gap-y-1 mb-6 w-[140px] h-[200px] justify-center border-white/10 border bg-blue-500/10 px-4 py-6 hover:border-white/50  hover:-translate-y-1 transition-all rounded-2xl ">
+                <div className="h-18">
+                  <img
+                    src={PostSQL}
+                    alt="SQL server"
+                    className="absolute top-6 left-7 w-20 h-20"
+                  />
+                </div>
+                <h4 className="text-md ">Postgresql</h4>
                 <div className="w-[100px] h-[6px] md:h-[7px] bg-gray-800 rounded  overflow-hidden">
                   <div
                     className={`w-0 h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] duration-700  ${
@@ -255,8 +278,6 @@ const About = () => {
                 </h4>
               </div>
 
-             
-
               <div className="relative flex flex-col items-center cursor-pointer gap-y-1 mb-6 w-[140px] h-[200px] justify-center border-white/10 border bg-blue-500/10 px-4 py-6 hover:border-white/50  hover:-translate-y-1 transition-all rounded-2xl ">
                 <div className="h-18">
                   <img
@@ -340,7 +361,6 @@ const About = () => {
                   {scrolled ? "90%" : "0%"}
                 </h4>
               </div>
-
             </div>
           </div>
 
