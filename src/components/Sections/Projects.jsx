2da02c886cd1e7092cf70fdefb9f169c88bc0bbe
Samrat-Deula7 <samratdeula461@gmail.com>
2026-05-19 08:46:07 +0545

    Added new project Team Track to the project section

--- src/components/Sections/Projects.jsx
+++ src/components/Sections/Projects.jsx
@@ -1,5 +1,7 @@
 import RevealOnScroll from "../../components/RevealOnScroll";
 import Project1 from "../../assets/Projects/E-Kam.png"
+import Porject2 from "../../assets/Projects/Team_Track.png"
+
 const Projects = () => {
   return (
     <section
@@ -7,14 +9,14 @@ const Projects = () => {
       className=" min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="max-w-5xl mx-auto px-4">
+        <div className="max-w-5xl mx-auto px-4 ">
           <h2 className="text-4xl md:text-6xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent text-center">
             Featured Projects
           </h2>
           {/* Projects are below */}
 
-          {/* {Project 1} */}
           <div className="grid  grid-cols-1 md:grid-cols-2 gap-6">
+            {/* {Project 1} */}
             {/* Each div is for an project */}
             <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]  transition-all">
               <div>
@@ -34,7 +36,7 @@ const Projects = () => {
                     >
                       {tech}
                     </span>
-                  )
+                  ),
                 )}
               </div>
 
@@ -42,6 +44,43 @@ const Projects = () => {
                 {/* Need to put link here */}
                 <a
                   href="https://e-kam.vercel.app/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  Live Demo ➜
+                </a>
+              </div>
+            </div>
+
+            {/* {Project 2} */}
+            <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]  transition-all">
+              <div>
+                <img src={Porject2} alt="Project1" />
+              </div>
+              <h3 className="text-xl font-bold mb-2 text-[#3b82f6]">
+                Team Track
+              </h3>
+              <p className="text-gray-400 mb-3">
+                Team Track is a modern project management web app designed to
+                help teams organize tasks, track progress, and collaborate
+                efficiently with a clean, user‑friendly interface.
+              </p>
+              <div className="flex flex-wrap">
+                {["React", "Node.js", "Postgresql", "Express.js","TailwindCSS","TypeScript"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 mx-1 my-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)]   transition-all"
+                    >
+                      {tech}
+                    </span>
+                  ),
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                {/* Need to put link here */}
+                <a
+                  href="https://team-track-3v5z.vercel.app/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   Live Demo ➜
